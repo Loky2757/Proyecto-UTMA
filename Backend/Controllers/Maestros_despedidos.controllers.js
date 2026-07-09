e2a6351bf96c31c4f6c46013e14a53cd8338a57f
Loky2757 <graciaf704@gmail.com>
2026-07-09 08:48:09 -0600

    Nuevo

--- Backend/Controllers/Maestros_despedidos.controllers.js
+++ Backend/Controllers/Maestros_despedidos.controllers.js
@@ -1,2 +1,2 @@
-import { tabla } from "../Models/Maestros_despedidos.model.js";
+import { tabla } from "../Models/Maestros_despedidos.models.js";
 tabla.create({name="Eliza",apepat="Mendez",sueldo=100,edad=30,materia="nada"})
--- Backend/Controllers/alumnos_Reprobados.controller.js
+++
@@ -1,2 +0,0 @@
-import { tabla2 } from "../Models/Alumnos_Reprobados.model.js";
-tabla2.create({nombre="Jose",apepat="Martinez",cal=9.5})
