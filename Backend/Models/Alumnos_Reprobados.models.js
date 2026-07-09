5c2a8796b2aa8905ac8d179d4947dc1a0eb6d70c
Loky2757 <graciaf704@gmail.com>
2026-07-09 08:51:01 -0600

    nuevo

--- Backend/Models/Alumnos_Reprobados.models.js
+++ Backend/Models/Alumnos_Reprobados.models.js
@@ -1,4 +1,4 @@
 import { Schema,Model } from "mongoose";
 
 const Esquema=new Schema({nombre=String,apepat=String,Cal=Number})
-export const tabla2 =new model ("tbla alumnos rebrobados ",Esquema )
+export const tabla2 =new model ("tabla alumnos rebrobados ",Esquema )
